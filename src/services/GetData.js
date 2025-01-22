87e2a7c195d04eee751a988fc7160b3ab8833482
hilo <7amzalaaroussi@gmail.com>
2025-01-22 12:53:22 +0100

    modify fetch URL

--- src/services/GetData.js
+++ src/services/GetData.js
@@ -1,7 +1,7 @@
 export const getData = async ()=>{
     //getting the cards data from a file in public folder /* test the fetching part */
     try {
-        const response = await fetch('/data/cards.json');
+        const response = await fetch('data/cards.json');
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
