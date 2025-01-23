c486856702d6245598db94493e75e1b0a5a14ed5
hilo <7amzalaaroussi@gmail.com>
2025-01-23 13:13:31 +0100

    modify the settings component

--- src/components/setting/Setting.jsx
+++ src/components/setting/Setting.jsx
@@ -12,25 +12,37 @@ function Setting({isOpen, resetGame}) {
     return (
         <div style={{border:"1px solid black" ,backgroundColor:"#ffff", height:"auto", width:"200px",position:"absolute",margin:"auto",paddingTop:"10px", borderRadius:"5px", gap:"5px",right:"65%",zIndex:"10" }}>
             {/* <p style={{marginRight:"10px"}}> */}
-            <div>
-                modes : 
-            {/* </p> */}
-                <select
-                    id="dropdown"
-                    value={mode}
-                    onChange={(e)=>{setMode(e.target.value); resetGame()}}
-                    style={{height: "30px", width:"70px", textAlign:"center"}}
-                >
-                    {/* <option value="">--Select an option--</option> */}
-                    <option value={4}>4 (2x2)</option>
-                    <option value={16}>16 (4x4)</option>
-                    {/* <option value={32}>32</option> */}
-                </select>
-            </div>
-            <div>
-                Background:
-                <input type='color' value={bgColor} style={{margin:"10px"}} onChange={(e)=>setBgColor(e.target.value)}/>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <td>
+                            modes:
+                        </td>
+                        <td>
+                        <select
+                            id="dropdown"
+                            value={mode}
+                            onChange={(e)=>{setMode(e.target.value); resetGame()}}
+                            style={{height: "30px", width:"70px", textAlign:"center"}}
+                        >
+                            {/* <option value="">--Select an option--</option> */}
+                            <option value={4}>4 (2x2)</option>
+                            <option value={16}>16 (4x4)</option>
+                            {/* <option value={32}>32</option> */}
+                        </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            background :
+                        </td>
+                        <td>
+                            <input type='color' value={bgColor} style={{margin:"10px"}} onChange={(e)=>setBgColor(e.target.value)}/>
+                        </td>
+                    </tr>
+                </thead>
+            </table>
+            
         </div>
     )
 }
